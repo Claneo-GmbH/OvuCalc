3b741f5630082b307a3d84e3b687ddeda2102321
VolodymyrDz <vdzhugan@brightgrove.com>
2025-03-17 11:56:27 +0200

    *fix calculations

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -2,11 +2,26 @@ let ovulationDays = [];
 let fertileDays = [];
 let initialDate = new Date();
 let lastDate = new Date();
-let fertileStart = new Date();
-let fertileEnd = new Date();
-let ovulationDate = new Date();
 
-document.getElementById("lastPeriod").addEventListener("change", function() {
+function restrictDateInput(inputId) {
+  let dateInput = document.getElementById(inputId);
+
+  let today = new Date();
+  let maxDate = today.toISOString().split('T')[0];
+
+  let minDate = new Date();
+  minDate.setDate(today.getDate() - 35);
+  minDate = minDate.toISOString().split('T')[0];
+
+  dateInput.setAttribute("max", maxDate);
+  dateInput.setAttribute("min", minDate);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  restrictDateInput("lastPeriod");
+});
+
+document.getElementById("lastPeriod").addEventListener("change", function () {
   initialDate = new Date(document.getElementById("lastPeriod").value);
   lastDate = new Date(document.getElementById("lastPeriod").value);
 });
@@ -49,25 +64,38 @@ function calculateOvulation() {
 }
 
 function showAnnotation() {
-  ovulationDate = new Date(ovulationDays[1]);
+  let ovulationDate = new Date(ovulationDays[1]);
+  let fertileStart = new Date(ovulationDate);
+  let fertileEnd = new Date(ovulationDate);
 
-  fertileStart.setDate(ovulationDate.getDate() - 5);
+  fertileStart.setDate(ovulationDate.getDate() - 4);
   fertileEnd.setDate(ovulationDate.getDate() + 1);
+
   document.getElementById("result").innerHTML = `
-                Nächsten Fruchtbare Tage: vom <strong>${fertileStart.toLocaleDateString("de-DE")}</strong> bis 
-                <strong>${fertileEnd.toLocaleDateString("de-DE")}</strong><br>
-                Eisprung: <strong>${ovulationDate.toLocaleDateString("de-DE")}</strong>
+                Nächsten Fruchtbare Tage: vom <strong>${formatDate(fertileStart)}</strong> bis 
+                <strong>${formatDate(fertileEnd)}</strong><br>
+                Eisprung: <strong>${formatDate(ovulationDate)}</strong>
             `;
 }
 
+function formatDate(date) {
+  let day = date.getDate().toString().padStart(2, '0');
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
+}
+
 function generateCalendar() {
-  console.log(ovulationDays, fertileDays)
+  const currentDate = new Date();
+  document.getElementById("prev-month").style.visibility = lastDate.getMonth() === currentDate.getMonth() - 1 ? 'hidden' : 'visible';
   document.getElementById("result-container").style.display = 'block';
+
   let calendarDiv = document.getElementById("calendar");
   calendarDiv.innerHTML = "";
 
   let monthYear = document.getElementById("monthYear");
-  monthYear.innerHTML = `${lastDate.getFullYear()} <span>${lastDate.toLocaleString('de-DE', { month: 'long' })}</span>`;
+  monthYear.innerHTML = `${lastDate.getFullYear()} <span>${lastDate.toLocaleString('de-DE', {month: 'long'})}</span>`;
 
   let calendar = document.createElement("table");
   calendar.style.width = "100%";
@@ -79,23 +107,33 @@ function generateCalendar() {
   let daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
   let prevMonthDays = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
   let row = document.createElement("tr");
+  let fertileFirstDay = true;
 
   for (let i = firstDay - 1; i >= 0; i--) {
     let cell = document.createElement("td");
     cell.textContent = prevMonthDays - i;
     cell.classList.add("prev-next-month");
+
+    let prevMonthDateObj = new Date(date.getFullYear(), date.getMonth() - 1, prevMonthDays - i + 1);
+
+    if (fertileDays.includes(prevMonthDateObj.toISOString().split('T')[0])) {
+      cell.classList.add(fertileFirstDay ? "fertile-first-day" : "fertile-day");
+      fertileFirstDay = false
+    }
+
     row.appendChild(cell);
   }
-  let fertileFirstDay = true;
+
   for (let i = 1; i <= daysInMonth; i++) {
     let cell = document.createElement("td");
-    let currentDateObj = new Date(date.getFullYear(), date.getMonth(), i);
+    let currentDateObj = new Date(date.getFullYear(), date.getMonth(), i+1);
     cell.textContent = i;
 
     if (fertileDays.includes(currentDateObj.toISOString().split('T')[0])) {
       cell.classList.add(fertileFirstDay ? "fertile-first-day" : "fertile-day");
       fertileFirstDay = false
     }
+
     if (ovulationDays.includes(currentDateObj.toISOString().split('T')[0])) {
       cell.classList.add("ovulation-day");
     }
@@ -112,6 +150,14 @@ function generateCalendar() {
     let cell = document.createElement("td");
     cell.textContent = i;
     cell.classList.add("prev-next-month");
+
+    let nextMonthDateObj = new Date(date.getFullYear(), date.getMonth() + 1, i + 1);
+
+    if (fertileDays.includes(nextMonthDateObj.toISOString().split('T')[0])) {
+      cell.classList.add("fertile-day");
+      fertileFirstDay = false
+    }
+
     row.appendChild(cell);
   }
 
@@ -121,8 +167,8 @@ function generateCalendar() {
 
 function changeMonth(offset) {
   lastDate.setMonth(lastDate.getMonth() + offset);
-  document.getElementById("prev-month").style.visibility = lastDate.getMonth() >= initialDate.getMonth() ? 'visible' : 'hidden' ;
-  document.getElementById("next-month").style.visibility = lastDate.getMonth() < initialDate.getMonth() + 5 ? 'visible' : 'hidden' ;
+  document.getElementById("prev-month").style.visibility = lastDate.getMonth() >= initialDate.getMonth() ? 'visible' : 'hidden';
+  document.getElementById("next-month").style.visibility = lastDate.getMonth() < initialDate.getMonth() + 5 ? 'visible' : 'hidden';
 
   generateCalendar();
 }
